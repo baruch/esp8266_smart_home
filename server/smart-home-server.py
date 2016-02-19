6d3b07d9dd7df057d509a6a9bf4c3a4c6f1df5d3
Igor <igrowing@gmail.com>
2016-02-19 22:45:06 +0200

    Fix for resolving of local IP
    The network interface is not always eth0. Sometines it is em0 or bond0
    or something else. The fix is universal and sort.

--- server/smart-home-server.py
+++ server/smart-home-server.py
@@ -31,16 +31,9 @@ except ImportError:
     import fcntl
     import struct
 
-    def get_ip_address(ifname):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        return socket.inet_ntoa(fcntl.ioctl(
-            s.fileno(),
-            0x8915,  # SIOCGIFADDR
-            struct.pack('256s', ifname[:15])
-        )[20:24])
-
     def get_server_ip():
-        return get_ip_address('eth0')
+        import socket
+        return socket.gethostbyname(socket.gethostname())
 
 class NodeList:
     def __init__(self):

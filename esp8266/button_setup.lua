74914cd46ef1de812b1fc197b1cdd7730c4df73c
Igor <igrowing@gmail.com>
2016-02-27 15:52:10 +0200

    Code review, comments, some fixes.

---
+++ esp8266/button_setup.lua
@@ -0,0 +1,22 @@
+-- LED-button GPIO index
+LBGI=LBGI or 5  -- GPIO 14
+INIT_DELAY = 5*1000*1000
+
+-- Process pressed button
+gpio.mode(LBGI, gpio.INPUT, gpio.PULLUP)  -- Set IO to Input pullup to off the LED.
+if gpio.read(LBGI) == 0 then -- button pressed
+	t1 = tmr.now()
+	tmr.alarm(5, 100, 1, function()
+		if gpio.read(LBGI) == 1 and (tmr.now() - t1 < INIT_DELAY) then -- Abort wait
+		    print("Button released.")
+			tmr.stop(5)
+			tmr.unregister(5)
+		elseif gpio.read(LBGI) == 0 and (tmr.now() - t1 >= INIT_DELAY) then -- Initialize the gadget
+			tmr.stop(5)
+			tmr.unregister(5)
+			print("Removing old settings and running setup.")
+			file.remove("netconfig.lc")
+			node.restart()
+		end
+	end)
+end
--- esp8266/wifi.lua
+++ esp8266/wifi.lua
@@ -12,8 +12,23 @@ local function run_setup()
     dofile ("server.lc")
 end
 
+local function non_nil(val)
+    if val == nil then
+        return ""
+    end
+end
+
 local function read_wifi_credentials()
-    return deserialize_file("netconfig.lc")
+    local c = deserialize_file("netconfig.lc")
+    c.wifi_desc = non_nil(c.wifi_desc)
+    c.wifi_ip = non_nil(c.wifi_ip)
+    c.wifi_ssid = non_nil(c.wifi_ssid)
+    c.wifi_password = non_nil(c.wifi_password)
+    c.wifi_nm = non_nil(c.wifi_nm)
+    c.wifi_gw = non_nil(c.wifi_gw)
+    c.wifi_dns = non_nil(c.wifi_dns)
+
+    return c
 end
 
 local function try_connecting(conf)
@@ -28,7 +43,7 @@ local function try_connecting(conf)
         wifi.sta.setip({ip=conf.wifi_ip, netmask=conf.wifi_nm, gateway=conf.wifi_gw})
     end
 
-    tmr.alarm(0, 1000, 1, function()
+    tmr.alarm(0, 2000, 1, function()
         if wifi.sta.status() ~= 5 then
           print("Connecting to AP...")
         else
@@ -42,12 +57,21 @@ local function try_connecting(conf)
         end
     end)
 
-    tmr.alarm(1, 10000, 0, function()
+    tmr.alarm(1, 20000, 0, function()
+        -- Sleep if sensor (save power until WiFi gets back), 
+        -- else run configuration mode
         if wifi.sta.status() ~= 5 then
             tmr.stop(0)
-            print("Failed to connect to \"" .. conf.wifi_ssid .. "\"")
-            run_setup()
             tmr.unregister(0)
+            print("Failed to connect to \"" .. wifi_ssid .. "\".")
+            if uclass == nil or uclass == "sensor" then 
+                print("Sleep 5 min + retry...")
+                print("Press the button 5 seconds on the next boot to enter WiFi configuration captive mode.")
+                -- No sense to run setup if the settings present. Sleep and retry.
+                node.dsleep(5 * 60 * 1000 * 1000, 0)   -- 5 min sleep            
+            else
+                run_setup()
+            end
         end
     end)
 end
@@ -57,10 +81,18 @@ end
 ------  MAIN  -----------
 -------------------------
 print('start')
+-- REVIEW: how to merge unit type and node params. Brainstorm together.
+if file.open("ut.lc", "r") then dofile("ut.lc")
+else print "Unit type is not set." end
+
+dofile("button_setup.lc")  -- uses timer #5
 wifi.sta.disconnect()
 print('disconnected')
 local conf = read_wifi_credentials()
 print('config read')
+-- TODO: Add your functionality here to do BEFORE connection established.
+--
+
 if conf.wifi_ssid ~= nil and conf.wifi_password ~= nil then
     print("Retrieved stored WiFi credentials")
     print("---------------------------------")
@@ -72,9 +104,12 @@ if conf.wifi_ssid ~= nil and conf.wifi_password ~= nil then
     print("wifi_dns : ", conf.wifi_dns)
     print("wifi_desc : ", conf.wifi_desc)
     _G.wifi_desc = conf.wifi_desc
+
+    -- set DNS to second slot if configured.
     if conf.wifi_dns ~= nil and conf.wifi_dns ~= '' then
         net.dns.setdnsserver(wifi_dns, 1)
     end
+
     try_connecting(conf)
     conf = nil
     collectgarbage()

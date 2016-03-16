b33eceebbe1f2d02727a784968b99e1b63fbdc2d
Baruch Even <baruch@ev-en.org>
2016-03-16 23:57:41 +0200

    Add function to easily set config without web interface

---
+++ esp8266/test_config.lua
@@ -0,0 +1,13 @@
+dofile('serialize.lc')
+
+function net_config(ssid, pw)
+    serialize_file("netconfig.lua", {
+        wifi_ssid = ssid,
+        wifi_password = pw,
+        wifi_ip = '',
+        wifi_nm = '',
+        wifi_gw = '',
+        wifi_dns = '',
+        wifi_desc = 'test config',
+    })
+end

93c36bc7995af75c934388028ae72fcb6e880c8f
Baruch Even <baruch@ev-en.org>
2016-03-21 23:54:32 +0200

    Add Arduino ESP8266 code
    For now this provides only the hotspot and initial configuration.

---
+++ arduino/arduino.ino
@@ -0,0 +1,150 @@
+#include <FS.h> //this needs to be first, or it all crashes and burns...
+#include "WiFiManager/WiFiManager.h"
+#include <ArduinoJson.h>
+#include "config.h"
+#include <GDBStub.h>
+
+#define CONFIG_FILE "/config.ini"
+
+char mqtt_server[40];
+char mqtt_port[6] = "9090";
+char static_ip[16] = "";
+char static_nm[16] = "";
+char static_gw[16] = "";
+char dns[40] = "";
+bool shouldSaveConfig;
+
+void print_str(const char *name, const char *val)
+{
+  Serial.print(name);
+  Serial.print(": \"");
+  Serial.print(val);
+  Serial.println('"');
+}
+
+void spiffs_mount() {
+  if (!SPIFFS.begin()) {
+    Serial.println("SPIFFs not initialized, reinitializing");
+    if (!SPIFFS.format()) {
+      Serial.println("SPIFFs format failed.");
+      while (1);
+    }
+  }
+}
+
+void config_load() {
+  Serial.println("SPIFFS initialized");
+  Config cfg(CONFIG_FILE);
+
+  Serial.println("Loading config");
+  cfg.readFile();
+  Serial.println("Config loaded");
+  strcpy(static_ip, cfg.getValueStr("ip"));
+  strcpy(static_gw, cfg.getValueStr("gw"));
+  strcpy(static_nm, cfg.getValueStr("nm"));
+  strcpy(static_nm, cfg.getValueStr("dns"));
+
+  print_str("IP", static_ip);
+  print_str("GW", static_gw);
+  print_str("NM", static_nm);
+  print_str("DNS", static_nm);
+}
+
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+}
+
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+void net_config() {
+  WiFiManager wifiManager;
+
+  WiFiManagerParameter custom_ip("ip", "Static IP", static_ip, 40);
+  WiFiManagerParameter custom_gw("gw", "Static Gateway", static_gw, 5);
+  WiFiManagerParameter custom_nm("nm", "Static Netmask", static_nm, 5);
+  WiFiManagerParameter custom_dns("dns", "Domain Name Server", dns, 40);
+  
+  if (strlen(static_ip) > 0) {
+    IPAddress _ip, _gw, _nm;
+    _ip.fromString(static_ip);
+    _gw.fromString(static_gw);
+    _nm.fromString(static_nm);
+    
+    wifiManager.setSTAStaticIPConfig(_ip, _gw, _nm);
+    
+  }
+
+  wifiManager.addParameter(&custom_ip);
+  wifiManager.addParameter(&custom_nm);
+  wifiManager.addParameter(&custom_gw);
+  wifiManager.addParameter(&custom_dns);
+
+  //wifiManager.setTimeout(600);
+  wifiManager.setAPCallback(configModeCallback);
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+  if (!wifiManager.autoConnect()) {
+    // Not connected
+    Serial.println("Failed to connect to AP");
+    delay(1000);
+    ESP.restart();
+  }
+
+  Serial.println("Connected");
+  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.gatewayIP());
+  Serial.println(WiFi.subnetMask());
+
+
+  //save the custom parameters to FS
+  if (shouldSaveConfig) {
+    Serial.println("saving config");
+    Config cfg(CONFIG_FILE);
+    cfg.setValueStr("ip", custom_ip.getValue());
+    cfg.setValueStr("gw", custom_gw.getValue());
+    cfg.setValueStr("nm", custom_nm.getValue());
+    cfg.setValueStr("dns", custom_dns.getValue());
+    cfg.writeFile();
+    
+    delay(100);
+    ESP.restart();
+    delay(1000);
+ }
+}
+
+void setup() {
+  Serial.begin(115200);
+#ifdef DEBUG
+  delay(10000); // Wait for port to open, debug only
+#endif
+
+  Serial.println(ESP.getResetInfo());
+  Serial.println(ESP.getResetReason());
+
+  spiffs_mount();
+  config_load();
+  net_config();
+
+  // Load node params
+  // Load wifi config
+  // Button reset check
+  Serial.println("Setup done");
+}
+
+void loop() {
+  if (Serial.available()) {
+    char ch = Serial.read();
+    if (ch == 'f') {
+      Serial.println("Clearing Config");
+      WiFi.disconnect(true);
+      SPIFFS.remove(CONFIG_FILE);
+      Serial.println("Clearing done");
+      delay(100);
+      ESP.restart();
+    }
+  }
+}
---
+++ arduino/config.ino
@@ -0,0 +1,230 @@
+#include "config.h"
+
+Config::Config(const char *filename)
+{
+  _filename = filename;
+  int i;
+  for (i = 0; i < NUM_CONFIG_ENTRIES; i++) {
+    types[i] = CONFIG_TYPE_UNKNOWN;
+    values[i] = NULL;
+    keys[i] = NULL;
+  }
+}
+
+Config::~Config() {
+  reset();
+}
+
+void Config::reset() {
+  int i;
+  for (i = 0; i < NUM_CONFIG_ENTRIES; i++) {
+    if (types[i] != CONFIG_TYPE_UNKNOWN && keys[i]) {
+      free(keys[i]);
+      keys[i] = NULL;
+
+      if (types[i] == CONFIG_TYPE_STRING) {
+        free(values[i]);
+      }
+      values[i] = NULL;
+      types[i] = CONFIG_TYPE_UNKNOWN;
+    }
+  }
+}
+
+bool Config::readFile() {
+  int ch;
+  bool res = true;
+
+  // Clear all existing values
+  reset();
+
+  // If file doesn't exist we are done
+  if (!SPIFFS.exists(_filename)) {
+    Serial.print("Config file ");
+    Serial.print(_filename);
+    Serial.println(" doesn't exist");
+    return false;
+  }
+  
+  File configFile = SPIFFS.open(_filename, "r");
+  ch = configFile.read();
+  if (ch == 'C') {
+    Serial.println("Config Error: Invalid header");
+    res = false;
+  }
+
+  while (res && configFile.available()) {
+    char t = configFile.read();
+    uint8_t l = configFile.read();
+
+    if (l == 0 || l > 31) {
+      Serial.print("Config Error: Invalid length ");
+      Serial.println(l);
+      res = false;
+      break;
+    }
+
+    char key[32];
+    configFile.read((uint8_t*)key, l);
+    key[l] = 0;
+
+    l = configFile.read();
+
+    switch (t) {
+      case 'I':
+        if (l == sizeof(int)) {
+          int val = 0;
+          configFile.read((uint8_t*)&val, sizeof(int));
+          setValueInt(key, val);
+        } else {
+          Serial.println("Config error: integer type not of sizeof(int)");
+          res = false;
+        }
+        break;
+      case 'S':
+        {
+          char value[64];
+          if (l > sizeof(value)-1) {
+            Serial.print("Config error: string length invalid");
+            Serial.println(l);
+            res = false;
+          } else {
+            configFile.read((uint8_t*)value, l);
+            value[l] = 0;
+            setValueStr(key, value);
+          }
+        }
+        break;
+      default:
+        Serial.print("Config Error: Unknown field type '");
+        Serial.print(t);
+        Serial.print("'=0x");
+        Serial.println(t, HEX);
+        res = false;
+        break;
+    }
+  }
+  
+  configFile.close();
+  return res;
+}
+
+bool Config::writeFile() {
+  int i;
+  File configFile = SPIFFS.open(_filename, "w");
+  configFile.write('C');
+  for (i = 0; i < NUM_CONFIG_ENTRIES; i++) {
+    switch (types[i]) {
+      case CONFIG_TYPE_UNKNOWN: break;
+      case CONFIG_TYPE_STRING:
+        {
+          uint8_t l;
+          configFile.write('S');
+
+          l = strlen(keys[i]);
+          configFile.write(l);
+          configFile.write((uint8_t*)keys[i], l);
+          
+          l = strlen((char*)values[i]);
+          configFile.write(l);
+          configFile.write((uint8_t*)values[i], l);
+        }
+        break;
+      case CONFIG_TYPE_INT:
+        configFile.write('I');
+        
+        uint8_t l = strlen(keys[i]);
+        configFile.write(l);
+        configFile.write((uint8_t*)keys[i], l);
+          
+        configFile.write(sizeof(int));
+        configFile.write((uint8_t*)&values[i], sizeof(int));
+        break;
+    }
+  }
+  configFile.flush();
+  configFile.close();
+}
+
+int Config::find_key(const char *key) {
+  int i;
+  for (i = 0; i < NUM_CONFIG_ENTRIES; i++) {
+    if (types[i] != CONFIG_TYPE_UNKNOWN && keys[i] && strcmp(keys[i], key)) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+ConfigType Config::getType(const char *key) {
+  int i = find_key(key);
+  if (i == -1)
+    return CONFIG_TYPE_UNKNOWN;
+  else
+    return (ConfigType)types[i];
+}
+
+bool Config::keyExists(const char *key) {
+  return find_key(key) != -1;
+}
+
+const char *Config::getValueStr(const char *key) {
+  int i = find_key(key);
+  if (i == -1)
+    return "";
+
+  if (values[i] == NULL)
+    return "";
+
+  return (const char *)values[i];
+}
+
+int Config::getValueInt(const char *key) {
+  int i = find_key(key);
+  if (i == -1)
+    return 0;
+
+  return (int)values[i];
+}
+
+int Config::allocate_entry(const char *key, ConfigType ctype) {
+  int i = find_key(key);
+  if (i == -1) {
+    for (i = 0; i < NUM_CONFIG_ENTRIES; i++) {
+      if (types[i] == CONFIG_TYPE_UNKNOWN)
+      break;
+    }
+    // No free entry found
+    if (i == NUM_CONFIG_ENTRIES) {
+      Serial.println("BUG: Failed to find empty space in config");
+      return -1;
+    }
+  }
+
+  if (types[i] == CONFIG_TYPE_UNKNOWN) {
+    keys[i] = strdup(key);
+    types[i] = ctype;
+  } else if (types[i] == CONFIG_TYPE_STRING) {
+    free(values[i]);
+    values[i] = NULL;
+  } else if (types[i] == CONFIG_TYPE_INT) {
+    values[i] = NULL;
+  }
+}
+
+void Config::setValueInt(const char *key, int value) {
+  int i = allocate_entry(key, CONFIG_TYPE_INT);
+  if (i == -1)
+    return;
+
+  values[i] = (void*)value;
+}
+
+void Config::setValueStr(const char *key, const char *value) {
+  int i = allocate_entry(key, CONFIG_TYPE_STRING);
+  if (i == -1)
+    return;
+
+ values[i] = strdup(value);
+}
+

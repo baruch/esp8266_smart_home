276e552a91b020f1e75e72d6d2a2eacd13b1634b
Baruch Even <baruch@ev-en.org>
2016-03-29 00:25:11 +0300

    Use bottle.py for the web server framework
    This will allow creating a small webapp to see the system status.

--- server/ota.py
+++ server/ota.py
@@ -1,43 +1,53 @@
 import threading
-import BaseHTTPServer
+from bottle import route, run, template, HTTPResponse, request, get, response
 
 HOST = '0.0.0.0'
 PORT = 24320
 
-class OTAHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+node_list = None
+otafile = None
+l = file('bottle.log', 'w')
 
-    def do_GET(self):
-        node = self.server.node_list.get_node_by_ip(self.client_address[0])
-        if node is None:
-            self.send_error(403, 'Unknown Node')
-            return
+def log(msg):
+    l.write(msg.__str__())
+    l.write('\n')
+    l.flush()
 
-        print self.headers
-
-        _version = self.headers.get('x-ESP8266-version')
-        _md5, content = self.server.otafile.check_update(_version)
-        if content is None:
-            self.send_error(304, 'Not Modified')
-            return
-
-        self.send_response(200)
-        self.send_header('Content-Length', len(content))
-        self.send_header('x-MD5', _md5)
-        self.end_headers()
-        self.wfile.write(content)
-        return
-
-
-class OTAServer(BaseHTTPServer.HTTPServer):
-    allow_reuse_address = True
+@get('/node_v1.bin')
+def node_v1_download():
+    node = node_list.get_node_by_ip(request.remote_addr)
+    log(request.remote_addr)
+    log(node)
+    if node is None:
+        log('403')
+        return HTTPResponse(status='403 Unknown Node')
     
-    def setup(self, node_list, otafile):
-        self.node_list = node_list
-        self.otafile = otafile
+    _version = request.get_header('x-ESP8266-version')
+    log(_version)
+    if _version is None:
+        log('version bad')
+        return HTTPResponse(status='403 Unknown version')
 
-def start(node_list, otafile):
-    server = OTAServer(('', PORT), OTAHandler)
-    server.setup(node_list, otafile)
-    server_thread = threading.Thread(target=server.serve_forever)
+    _md5, content = otafile.check_update(_version)
+    log(_md5)
+    if content is None:
+        log('no content')
+        return HTTPResponse(status='304 Not Modified')
+
+    log('normal')
+    log('content len %d' % len(content))
+    response.set_header('x-MD5', _md5)
+    return content
+
+@get('/')
+def index():
+    return 'Hello World!'
+
+def start(_node_list, _otafile):
+    global node_list, otafile
+    node_list = _node_list
+    otafile = _otafile
+
+    server_thread = threading.Thread(target=run, kwargs=dict(host=HOST, port=PORT))
     server_thread.daemon = True
     server_thread.start()

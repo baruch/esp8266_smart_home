16436d9e7a6a6940dfdbe0e301719bb146f732f3
Baruch Even <baruch@ev-en.org>
2016-03-29 00:26:23 +0300

    Remove debug logs

--- server/ota.py
+++ server/ota.py
@@ -6,36 +6,23 @@ PORT = 24320
 
 node_list = None
 otafile = None
-l = file('bottle.log', 'w')
-
-def log(msg):
-    l.write(msg.__str__())
-    l.write('\n')
-    l.flush()
 
 @get('/node_v1.bin')
 def node_v1_download():
     node = node_list.get_node_by_ip(request.remote_addr)
-    log(request.remote_addr)
-    log(node)
     if node is None:
-        log('403')
         return HTTPResponse(status='403 Unknown Node')
     
     _version = request.get_header('x-ESP8266-version')
     log(_version)
     if _version is None:
-        log('version bad')
         return HTTPResponse(status='403 Unknown version')
 
     _md5, content = otafile.check_update(_version)
     log(_md5)
     if content is None:
-        log('no content')
         return HTTPResponse(status='304 Not Modified')
 
-    log('normal')
-    log('content len %d' % len(content))
     response.set_header('x-MD5', _md5)
     return content
 

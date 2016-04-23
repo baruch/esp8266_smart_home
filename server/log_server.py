357e77334c164b6e21f7c0205254d4b6d05fdcb1
Baruch Even <baruch@ev-en.org>
2016-04-24 00:05:05 +0300

    log server: emit message when a client (dis)connects

--- server/log_server.py
+++ server/log_server.py
@@ -8,6 +8,7 @@ PORT = 24321
 class LogHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         client_address = self.client_address[0]
+        self.server.node_list.log(client_address, "Server: Node connect to log server")
         while 1:
             try:
                 data = self.request.recv(4096)
@@ -15,15 +16,21 @@ class LogHandler(SocketServer.BaseRequestHandler):
                 break
             if not data: break
             self.server.node_list.log(client_address, data)
+        self.server.node_list.log(client_address, "Server: Node disconnected from log server")
+        try:
+            self.request.shutdown(socket.SHUT_RDWR)
+        except IOError:
+            pass
 
 class LogServer(SocketServer.ThreadingTCPServer):
-    def set_node_list(self, node_list):
+    def __init__(self, address, handler, node_list):
+        SocketServer.ThreadingTCPServer.__init__(self, address, handler)
+        self.allow_reuse_address = True
         self.node_list = node_list
 
 def start(node_list):
     print 'Starting log server on port ', PORT
-    server = LogServer((HOST, PORT), LogHandler)
-    server.set_node_list(node_list)
+    server = LogServer((HOST, PORT), LogHandler, node_list)
     server_thread = threading.Thread(target=server.serve_forever)
     server_thread.daemon = True
     server_thread.start()
